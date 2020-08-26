0a5ff1f7acc447d6f78bef4ab5f2e41bebf16a4d
michealccc <4558199+michealccc@users.noreply.github.com>
2020-08-26 16:04:18 +1200

    Added mocha and chai testing framework

---
+++ test/test.test.js
@@ -0,0 +1,11 @@
+var expect    = require("chai").expect;
+
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            var arrayIndex = [1, 2, 3];
+
+            expect(arrayIndex.indexOf(4)).to.equal(-1);
+        });
+    });
+});
